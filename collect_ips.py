afae296fec81f0c1d01627623da3ea2cfc5aa119
rchsw5vq <94264382+rchsw5vq@users.noreply.github.com>
2024-12-25 20:55:00 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -25,7 +25,7 @@ with open('ip.txt', 'w') as file:
         soup = BeautifulSoup(response.text, 'html.parser')
         
         # 根据网站的不同结构找到包含IP地址的元素
-        if url == 'https://ip.164746.xyz/ipTop10.html':
+        if url == 'https://api.uouin.com/cloudflare.html':
             elements = soup.find_all('tr')
         elif url == 'https://cf.090227.xyz':
             elements = soup.find_all('tr')
